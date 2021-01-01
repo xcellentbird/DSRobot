ab5aec6644b8b3bb2f62584656b91746da55c38d
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-01-02 00:24:38 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import threading
-from what_food import yolo4food
+from food_recog import yolo4food
+from table2serve import table2serve
 
 
 if __name__=='__main__':
-    yolo = yolo4food()
+    yolo = yolo4food() # yolo init
+    t2s = table2serve() 
     # cam_num=4, txt='food_out', showmode=False, writemode=False, detect_term = 5
     cam_thrd = threading.Thread(target=yolo.cam_on, args=(2, 'food_out.txt', True, False, 2))
+    order_thrd = threading.Thread(target=t2s.order_update, args=(yolo.get_food()))
+
     cam_thrd.start()
+    order_thrd.start()
