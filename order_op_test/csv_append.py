2722055ca8bbe982791269c2d602fea604c9e526
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-01-13 21:55:58 +0900

    Add files via upload

--- order_op_test/csv_append.py
+++ order_op_test/csv_append.py
@@ -1,18 +1,43 @@
+#args test
+import argparse
 import csv
 
 jja = 'Jja-jang-myoen'
 jjam = 'Jjam-bbong'
 tang = 'Tang-su-yuk'
-while(True):
-    print("테이블 번호(1~4)와 음식(1=짜장, 2=짬, 3=탕)을 입력하세요")
-    ol = list(map(int,input().split()))
+menu = [jja, jjam, tang]
+table = [1, 2, 3, 4]
 
-    if ol[0] == 0 or ol[1] == 0: break
+def parser():
+    parser = argparse.ArgumentParser(description="add order... table: 1,2,3,4  menu: Jja-jang, Jjam-bbong, Tang-su-yuk")
+    parser.add_argument("table_num", type=int, default=1, help='table number')
+    parser.add_argument("food_name", type=str, default='jja-jang', help='food name')
+    args = parser.parse_args()
     
-    if ol[1] == 1: ol[1] = jja
-    elif ol[1] == 1: ol[1] = jjam
-    else: ol[1] = tang
+    return args.table_num, args.food_name
 
-    with open('order_pandas.csv','a', encoding='utf-8', newline='') as csv_file:
+def check_parser(table_number, food_name):
+    if table_number not in table:
+        print("there is no {} table".format(table_number))
+        print("table number: 1, 2, 3, 4")
+        return False
+    elif food_name not in menu:
+        print("{} is not in our menu".format(food_name))
+        print("menu: Jja-jang, Jjam-bbong, Tang-su-yuk")
+        return False
+    else:
+        return True
+
+def add_order(table_number, food_name, csv_name='order_pandas.csv'):
+    with open(csv_name,'a', encoding='utf-8', newline='') as csv_file:
         appender = csv.writer(csv_file)
-        appender.writerow(ol)
+        appender.writerow([table_number, food_name])
+    print("order added!")
+
+if __name__=='__main__':
+    tn, fn = parser()
+    if check_parser(tn, fn):
+        print(tn,"- table")
+        print(fn,"- food")
+        add_order(tn, fn)
+        
