32cbf730408bc37b94cb30da2b9457dc9e645549
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-01-13 21:56:50 +0900

    Add files via upload

--- order_op_test/csv_pandas_test.py
+++ order_op_test/csv_pandas_test.py
@@ -2,18 +2,19 @@ import csv
 import numpy as np
 import pandas as pd
 
-jja = 'Jja-jang-myoen'
+jja = 'Jja-jang-myeon'
 jjam = 'Jjam-bbong'
 tang = 'Tang-su-yuk'
+menu = [jja, jjam, tang]
 
-def table2serve(food):
-    data = pd.read_csv("order_pandas.csv", names=['table number', 'food'])
+def table2serve(food, csv_name='order_pandas.csv'):
+    data = pd.read_csv(csv_name, names=['table number', 'food'])
     flg = False
     table = 0
     data = data.values.tolist()
     for i, row in enumerate(data):
         if food in row:
-            print(food +" is in ",i," order")
+            #print(food +" is in ",i,"th order")
             table = row[0]
             del data[i]
             flg = True
@@ -23,8 +24,24 @@ def table2serve(food):
         return -1
     
     data = pd.DataFrame(np.array(data))
-    data.to_csv("order_pandas.csv", index=False, header=False)
+    data.to_csv(csv_name, index=False, header=False)
     print("order updated")
     return table
-    
-print(table2serve(jja),"번 테이블로 서빙합니다")
+
+if __name__ == "__main__":
+    print("key 'q' - exit")
+    while True:
+        print("menu: Jja-jang-myeon, Jjam-bbong, Tang-su-yuk")
+        print("menu input: ")
+        ipt = input()
+        if ipt == 'q':
+            print("exit~")
+        elif ipt not in menu:
+            print(ipt,"is not on the list")
+        else:
+            t2s = table2serve(ipt)
+            if t2s == -1:
+                print("There is no table ordered ",ipt)
+            else:
+                print(t2s,"번 테이블로 서빙합니다")
+        
