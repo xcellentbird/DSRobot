21b4bf25ae954a9cba4158e7582447ca79664b5b
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-01-14 21:38:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
 import threading
+import csv
+import numpy as np
+import pandas as pd
 from food_recog import yolo4food
-from table2serve import table2serve
 
+jja = 'Jja-jang-myeon'
+jjam = 'Jjam-bbong'
+tang = 'Tang-su-yuk'
+menu = [jja, jjam, tang]
+
+def table2serve(food, csv_name='orders.csv'):
+    data = pd.read_csv(csv_name, names=['table number', 'food'])
+    flg = False
+    table = 0
+    data = data.values.tolist()
+    for i, row in enumerate(data):
+        if food in row:
+            #print(food +" is in ",i,"th order")
+            table = row[0]
+            del data[i]
+            flg = True
+            break
+    if not flg:
+        print(food + " is not in order!!")
+        return -1
+    
+    data = pd.DataFrame(np.array(data))
+    data.to_csv(csv_name, index=False, header=False)
+    print("order updated")
+    return table
 
 if __name__=='__main__':
     yolo = yolo4food() # yolo init
-    t2s = table2serve() 
     # cam_num=4, txt='food_out', showmode=False, writemode=False, detect_term = 5
     cam_thrd = threading.Thread(target=yolo.cam_on, args=(2, 'food_out.txt', True, False, 2))
-    order_thrd = threading.Thread(target=t2s.order_update, args=(yolo.get_food()))
+    order_thrd = threading.Thread(target=table2serve, args=(yolo.get_food(), 'orders.csv'))
 
     cam_thrd.start()
     order_thrd.start()
+
+    print("key 'q' - exit")
+    while True:
+        print("menu: Jja-jang-myeon, Jjam-bbong, Tang-su-yuk")
+        print("menu input: ")
+        ipt = input()
+        if ipt == 'q':
+            print("exit~")
+        elif ipt not in menu:
+            print(ipt,"is not on the list")
+        else:
+            t2s = table2serve(ipt)
+            if t2s == -1:
+                print("There is no table ordered ",ipt)
+            else:
+                print(t2s,"번 테이블로 서빙합니다")
