eff5d479303f076792df662eb635a0ec704c40c4
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2021-01-31 17:44:07 +0900

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 import sys
+import os
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt, QTimer, QTime, QTimerEvent
+from PyQt5.QtCore import Qt, QTimer, QTime, QTimerEvent, QDate, QCoreApplication
 import time
 import csv
 
@@ -20,28 +21,34 @@ class TableModel(QtCore.QAbstractTableModel):
         return len(self._data[0])
     
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, renew = 1):
+    def __init__(self):
         super().__init__()
-        self.cnt = 0
-        self.data = []
 
-        self.setWindowTitle('받은 주문')
-        
         self.table = QtWidgets.QTableView()
         self.setCentralWidget(self.table)
 
+        self.timebar = self.statusBar()
+        
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.order_update)
-        self.timer.start(renew * 1000)
+        self.timer.timeout.connect(self.updat)
+        self.timer.start(1000)
         self.show()
 
-    def order_update(self):
-        with open('orders.csv', 'r', encoding='utf-8', newline='') as csv_file:
-            d = list(csv.reader(csv_file))
-        self.data = d
-        self.model = TableModel(self.data)
-        self.table.setModel(self.model)
 
+    def updat(self):
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/orders.csv', 'r', encoding='utf-8', newline='') as csv_file:
+            data = list(csv.reader(csv_file))
+            self.table.setFont(QtGui.QFont('SansSerif', 15))
+
+            self.table.setModel(TableModel(data))
+            
+        self.show_timebar()
+        
+
+    def show_timebar(self):
+        date = QDate.currentDate().toString(Qt.DefaultLocaleLongDate)
+        time = QTime.currentTime().toString(Qt.DefaultLocaleLongDate)
+        self.timebar.showMessage(date + time)
 
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
